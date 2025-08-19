9000977db6f49eb0cf60c13fb9c311d50ef4055f
Dloed <96358595+Dloed@users.noreply.github.com>
2025-08-19 05:55:00 +0200

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,5 +1,5 @@
 // src/api.js
-const BASE_URL = import.meta.env?.VITE_API_URL || 'http://localhost:3000';
+const BASE_URL =  'http://localhost:3000';
 
 function makeUrl(path) {
   return path.startsWith('http') ? path : BASE_URL + path;
